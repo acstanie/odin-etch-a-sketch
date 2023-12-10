d12ca277bee1dde5a0b457d53ee9f5e5ad11fc26
acstanie <alexhartman95@gmail.com>
2023-12-09 23:22:42 -0400

    Created the grid in a updatable container

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 function createContainer() {
     const body = document.querySelector('body');
+    const gridContainer = document.createElement('div');
     const container = document.createElement('div');
+    const clearBtn = document.createElement('button');
+
     container.classList.toggle('container');
+    gridContainer.classList.toggle('grid-container');
+    clearBtn.setAttribute('id', 'clear-btn');
+    clearBtn.textContent = 'Clear';
+
+    container.appendChild(clearBtn);
     body.appendChild(container);
+    container.appendChild(gridContainer);
 }
 
 function createGrid() {
-    const container = document.querySelector('.container');
-    let dimension = 25;
+    const gridContainer = document.querySelector('.grid-container');
+    let row = 2;
+    let col = 2;
 
-    for(let i = 0; i < dimension; i++) {
+    for(let i = 0; i < col; i++) {
         const rowDiv = document.createElement('div');
         rowDiv.classList.toggle('row-div');
-        container.appendChild(rowDiv);
-        for(let j = 0; j < dimension; j++) {
+        gridContainer.appendChild(rowDiv);
+        for(let j = 0; j < row; j++) {
             const colDiv = document.createElement('div');
             colDiv.classList.toggle('col-div');
             rowDiv.appendChild(colDiv);
         }  
     }
+}
+
+function clearGrid() {
+    const clearBtn = document.querySelector('#clear-btn');
+    const colDiv = document.querySelectorAll('.col-div');
+
+    clearBtn.addEventListener('click', (e) => {
+        for(let i = 0; i < colDiv.length; i++) {
+            colDiv[i].style.backgroundColor = 'white';
+        }
+
+    });
 }
 
 function makeSketch() {
@@ -28,10 +50,12 @@ function makeSketch() {
     const colDiv = document.querySelectorAll(".col-div");
 
     for(let i = 0; i < colDiv.length; i++) {
-        colDiv[i].addEventListener('dragover', (e) => {
+            colDiv[i].addEventListener('dragover', (e) => {
             colDiv[i].style.backgroundColor = 'black';
         });
     }
+
+    clearGrid();
 }
 
 makeSketch();
